57359ce8b34e78ae54a8b04c80c594f14cfc8511
Geoffrey Rogers <rogersg@usc.edu>
2026-02-18 17:37:40 -0800

    more fixed log entries to go before export...

--- R/global.R
+++ R/global.R
@@ -275,6 +275,11 @@ tas_analyze <- function(InputFilePath,
             files = str_c(WD,c("Export","filtered","logs","merged")),
             root = WD)
 
+  t14 <- Sys.time()
+  cat(str_c("ZIP archive of results generated. Elapsed time: ",format(t14-t13, digits = 4, with.units = TRUE)), file = paste0(WD,"logs/tasAnalyzer logs.txt"), sep = "\n", append = TRUE)
+  tf <- Sys.time()
+  cat(str_c("\nAnalysis complete. Total elapsed time: ",format(tf-t0, digits = 4, with.units = TRUE)), file = paste0(WD,"logs/tasAnalyzer logs.txt"), sep = "\n", append = TRUE)
+
   if (shiny.env){
     incProgress(amount = 8/30, message = 'Done!')
   }else{
@@ -282,11 +287,6 @@ tas_analyze <- function(InputFilePath,
                 str_c(substr(InputFilePath,1,str_locate(InputFilePath,"Input.csv")[,"start"]-1),"analyzed.zip"),
                       overwrite = TRUE)
   }
-  t14 <- Sys.time()
-
-  cat(str_c("ZIP archive of results generated. Elapsed time: ",format(t14-t13, digits = 4, with.units = TRUE)), file = paste0(WD,"logs/tasAnalyzer logs.txt"), sep = "\n", append = TRUE)
-  tf <- Sys.time()
-  cat(str_c("\nAnalysis complete. Total elapsed time: ",format(tf-t0, digits = 4, with.units = TRUE)), file = paste0(WD,"logs/tasAnalyzer logs.txt"), sep = "\n", append = TRUE)
 
   return(Results)
 }
